9c8a09dae241dab625f4d44ab228b377d75b1e57
Thomas Leonard <tml1957@rit.edu>
2022-11-13 17:23:08 -0500

    Domo-Maker D Fix

--- client/maker.jsx
+++ client/maker.jsx
@@ -8,18 +8,18 @@ const handleDomo = (e) => {
     const age = e.target.querySelector('#domoAge').value;
     const _csrf = e.target.querySelector('#_csrf').value;
 
-    if(!name || !age) {
-        helper.handleError('All fields required!');
+    if (!name || !age) {
+        helper.handleError('All fields are required!');
         return false;
     }
 
     helper.sendPost(e.target.action, {name, age, _csrf}, loadDomosFromServer);
 
     return false;
-}
+};
 
 const DomoForm = (props) => {
-    return (
+    return ( 
         <form id="domoForm"
             onSubmit={handleDomo}
             name="domoForm"
@@ -28,10 +28,10 @@ const DomoForm = (props) => {
             className="domoForm"
         >
             <label htmlFor="name">Name: </label>
-            <input id='domoName' type="text" name="name" placeholder="Domo Name" />
+            <input type="text" id="domoName" name="name" placeholder="Domo Name" />
             <label htmlFor="age">Age: </label>
-            <input id="domoAge" type="number" min="0" name="age" />
-            <input id="_csrf" type="hidden" name="_csrf" value={props.csrf} />
+            <input type="number" id="domoAge" name="age" min="0" />
+            <input type="hidden" id="_csrf" name="_csrf" value={props.csrf} />
             <input className="makeDomoSubmit" type="submit" value="Make Domo" />
         </form>
     );
@@ -41,7 +41,7 @@ const DomoList = (props) => {
     if (props.domos.length === 0) {
         return (
             <div className="domoList">
-                <h3 className="emptyDomo">No Domos Yet!</h3>
+                <h3 className='emptyDomo'>No Domos Yet!</h3>
             </div>
         );
     }
@@ -49,19 +49,19 @@ const DomoList = (props) => {
     const domoNodes = props.domos.map(domo => {
         return (
             <div key={domo._id} className="domo">
-                <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
-                <h3 className="domoName">Name: {domo.name} </h3>
-                <h3 className="domoAge">Age: {domo.age} </h3>
+                <img src="/assets/img/domoface.jpeg" alt="domo face" className='domoFace' />
+                <h3 className='domoName'>Name: {domo.name} </h3>
+                <h3 className='domoAge'>Age: {domo.age} </h3>
             </div>
         );
     });
 
     return (
-        <div className="domoList">
+        <div className='domoList'>
             {domoNodes}
         </div>
     );
-}
+};
 
 const loadDomosFromServer = async () => {
     const response = await fetch('/getDomos');
@@ -70,21 +70,23 @@ const loadDomosFromServer = async () => {
         <DomoList domos={data.domos} />,
         document.getElementById('domos')
     );
-}
+};
 
 const init = async () => {
     const response = await fetch('/getToken');
     const data = await response.json();
 
-    ReactDom.render(
-        <DomoList csrf={data.csrfToken} />,
+    ReactDOM.render(
+        <DomoForm csrf={data.csrfToken} />,
         document.getElementById('makeDomo')
     );
 
-    ReactDom.render(
+    ReactDOM.render(
         <DomoList domos={[]} />,
         document.getElementById('domos')
     );
 
     loadDomosFromServer();
-}
+};
+
+window.onload = init;
